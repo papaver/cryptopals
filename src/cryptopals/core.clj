76da08589d2b1f45fd9998119adb459a56fa429b
Moiz Merchant <moiz@electronicdreams>
2017-08-29 15:13:04 -0600

    Set 1: Challenge 1: Convert hex to base64.

--- src/cryptopals/core.clj
+++ src/cryptopals/core.clj
@@ -1,7 +1,14 @@
 (ns cryptopals.core
+  (:import
+    (org.apache.commons.codec.binary Base64 Hex))
   (:gen-class))
 
+(defn hex->base64
+  "Convert hex to base64."
+  [hex]
+  (Base64/encodeBase64String (Hex/decodeHex (.toCharArray hex))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  ""
   [& args]
-  (println "Hello, World!"))
+  (println "Nothing here."))
--- test/cryptopals/core_test.clj
+++ test/cryptopals/core_test.clj
@@ -2,6 +2,7 @@
   (:require [clojure.test :refer :all]
             [cryptopals.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest hex->base64-test
+  (testing "Convert hex to base64"
+    (is (= "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+           (hex->base64 "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")))))
