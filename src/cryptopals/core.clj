3d4c6ace4877b01172085cfc493b0582775958f8
Moiz Merchant <moiz@electronicdreams>
2017-08-30 01:29:30 -0600

    Set 1: Challenge 3: Single-byte XOR cipher.

--- src/cryptopals/core.clj
+++ src/cryptopals/core.clj
@@ -1,20 +1,78 @@
 (ns cryptopals.core
+  (:require [clojure.string :as string])
   (:import
     (org.apache.commons.codec.binary Base64 Hex))
   (:gen-class))
 
-(defn hex->bytes [hex]
-  "Converts string hex to byte[]."
-  (Hex/decodeHex (.toCharArray hex)))
+;;- utils ---------------------------------------------------------------------
 
-(defn bytes->hex [bytes]
-  "Converts byte[] to string hex."
-  (apply str (Hex/encodeHex bytes)))
+(defn hex->bytes
+  "Converts hex string to byte[]."
+  ([]
+   (fn [rf]
+     (fn
+       ([] (rf))
+       ([result] (rf result))
+       ([result input] (reduce rf result (hex->bytes input))))))
+  ([hex]
+   (Hex/decodeHex (.toCharArray hex))))
+
+(defn bytes->hex
+  "Converts byte[] to hex string."
+  [bs]
+  (apply str (Hex/encodeHex bs)))
+
+(defn int->hex
+  "Converts an integer into string hex."
+  [i]
+  (bytes->hex (byte-array [i])))
+
+(defn byte->int
+  "Converts a byte into an integer."
+  [b]
+  (bit-and 0xff b))
+
+(def byte->ascii
+  "Convert byte to ascii."
+  (comp
+    char
+    byte->int))
+
+(defn hex->ascii
+  "Convert hex string to ascii string."
+  [hex]
+  (apply str (into [] (comp (hex->bytes)
+                      (map byte->ascii)) [hex])))
+
+(defn map-values [f m]
+  (into {} (map (fn [[k v]] {k (f v)})) m))
+
+(defn a-z?
+  "Character is between a and z."
+  [c]
+  (let [x (int c)]
+    (and (<= 97 x) (<= x 122))))
+
+(defn letter-frequencies
+  "Calculate frequencies of english letters in string."
+  [s]
+  (let [ls (filter a-z? (string/lower-case s))]
+    (frequencies ls)))
+
+(defn sqr-err
+  "Calculate the sum of the error squared."
+  [observed expected]
+  (let [diff (- observed expected)]
+    ((comp double /) (* diff diff) expected)))
+
+;;- set 1: challenge 1 --------------------------------------------------------
 
 (defn hex->base64
-  "Convert string of hex to base64."
+  "Convert string hex to base64."
   [hex]
   (Base64/encodeBase64String (hex->bytes hex)))
+
+;;- set 1: challenge 2 --------------------------------------------------------
 
 (defn fixed-xor
   "Takes two equal-length buffers and produces their XOR combination."
@@ -24,6 +82,46 @@
         bytes-y (hex->bytes y)
         zipped (map vector bytes-x bytes-y)]
     (bytes->hex (byte-array (map (partial apply bit-xor) zipped)))))
+
+;;- set 1: challenge 3 --------------------------------------------------------
+
+(def en-letter-frequencies
+  "English language letter frequencies.
+   reference: https://en.wikipedia.org/wiki/Letter_frequency"
+  {\a 0.08167 \b 0.01492 \c 0.02782 \d 0.04253 \e 0.12702 \f 0.02228 \g 0.02015
+   \h 0.06094 \i 0.06966 \j 0.00153 \k 0.00772 \l 0.04025 \m 0.02406 \n 0.06749
+   \o 0.07507 \p 0.01929 \q 0.00095 \r 0.05987 \s 0.06327 \t 0.09056 \u 0.02758
+   \v 0.00978 \w 0.02360 \x 0.00150 \y 0.01974 \z 0.00074})
+
+(defn is-english?-score
+  "Use chi-squared testing to score a string as english."
+  [s]
+  (try (let [s-freqs (letter-frequencies s)
+             s-len (reduce + (vals s-freqs))
+             en-freq-err (fn [k v] (sqr-err (get s-freqs k 0) (* s-len v)))]
+         (reduce-kv #(+ %1 (en-freq-err %2 %3)) 0 en-letter-frequencies))
+       (catch Exception e nil)))
+
+(defn single-byte-xor-cipher
+  "A hex encoded string has been XOR'd against a single character.
+  Find the key, decrypt the message."
+  [hex]
+  (let [hex-len (bit-shift-right (count hex) 1)]
+    (letfn [(repeat-xor-hex [xh]
+              (apply str (take hex-len (repeat xh))))
+            (score-test [s]
+              {:msg s
+               :score (is-english?-score s)})]
+      (let [xf (comp (map int->hex)
+                     (map repeat-xor-hex)
+                     (map (partial fixed-xor hex))
+                     (map hex->ascii)
+                     (map score-test)
+                     (remove #(nil? (:score %))))]
+        (->> (into [] xf (range 0 256))
+             (sort-by #(:score %))
+             first
+             :msg)))))
 
 (defn -main
   ""
--- test/cryptopals/core_test.clj
+++ test/cryptopals/core_test.clj
@@ -2,6 +2,60 @@
   (:require [clojure.test :refer :all]
             [cryptopals.core :refer :all]))
 
+;;- utils ---------------------------------------------------------------------
+
+(deftest hex->bytes-test
+  (testing "Convert hex to byte[]"
+    (is (= [0] (map int (hex->bytes "00"))))
+    (is (= [-1] (map int (hex->bytes "ff"))))))
+
+(deftest bytes->hex-test
+  (testing "Convert byte[] to hex"
+    (is (= "00" (bytes->hex (byte-array [0]))))
+    (is (= "ff" (bytes->hex (byte-array [-1]))))))
+
+(deftest int->hex-test
+  (testing "Convert int to hex"
+    (is (= "00" (int->hex 0)))
+    (is (= "ff" (int->hex 255)))))
+
+(deftest byte->int-test
+  (testing "Convert byte to int"
+    (is (= 0 (byte->int (byte 0))))
+    (is (= 255 (byte->int (byte -1))))))
+
+(deftest hex->ascii-test
+  (testing "Convert hex to ascii string"
+    (is (= "ab" (hex->ascii "6162")))))
+
+(deftest map-values-test
+  (testing "Transforming values in a map"
+    (is (= {:a 2 :b 3}
+           (map-values inc {:a 1 :b 2})))))
+
+(deftest a-z?-test
+  (testing "Test for lowercase english characters"
+    (is (a-z? \a))
+    (is (not (a-z? \A)))
+    (is (not (a-z? \?)))))
+
+(deftest letter-frequencies-test
+  (testing "English letter frequencies"
+    (is (= {\b 3
+            \c 4
+            \d 6
+            \e 12}
+          (letter-frequencies "bbb CCCC!dddddd_eeeeeeeeeeee")))))
+
+(deftest sqr-err-test
+  (testing "Squared error test"
+    (is (= 0.0 (sqr-err 5 5)))
+    (is (= 0.2 (sqr-err 4 5)))
+    (is (= 1.8 (sqr-err 2 5)))
+    (is (= 3.2 (sqr-err 1 5)))))
+
+;;- set 1 ---------------------------------------------------------------------
+
 (deftest hex->base64-test
   (testing "Convert hex to base64"
     (is (= "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
@@ -12,3 +66,8 @@
     (is (= "746865206b696420646f6e277420706c6179"
            (fixed-xor "1c0111001f010100061a024b53535009181c"
                       "686974207468652062756c6c277320657965")))))
+
+(deftest single-byte-xor-cipher-test
+  (testing "Single-byte XOR cipher"
+    (is (= "Cooking MC's like a pound of bacon"
+           (single-byte-xor-cipher "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")))))
